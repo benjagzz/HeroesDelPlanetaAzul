9a8905ac9037c6e108093df6ca5d4fb895266fa4
bengzz <benjagzz@gmail.com>
2014-03-27 21:12:08 -0600

    Update AjustesViewController.h

--- HeroesDelPlanetaAzul/AjustesViewController.h
+++ HeroesDelPlanetaAzul/AjustesViewController.h
@@ -2,9 +2,9 @@
 //  AjustesViewController.h
 //  HeroesDelPlanetaAzul
 //
-//  Created by Ari Flores on 26/03/14.
+//  Created by Ari Flores on 
 //  Copyright (c) 2014 AriFlores. All rights reserved.
-//
+// 
 
 #import <UIKit/UIKit.h>
 
