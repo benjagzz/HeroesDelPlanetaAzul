600a6b40a2249affb7cd6b98422c2f2a5f45732c
ariflores <ari926@hotmail.com>
2014-04-20 14:51:40 -0500

    Se muestran los espacios vacíos en el juego de acuerdo a la frase (la frase es dada en el código, aún no la carga del Core Data), pero si se le pica a una letra la busca y despliega los espacios con esa letra

--- HeroesDelPlanetaAzul/JuegoViewController.h
+++ HeroesDelPlanetaAzul/JuegoViewController.h
@@ -9,7 +9,49 @@
 #import <UIKit/UIKit.h>
 
 @interface JuegoViewController : UIViewController
+
 @property (weak, nonatomic) IBOutlet UILabel *fraseLabel;
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView1;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView2;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView3;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView4;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView5;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView6;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView7;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView8;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView9;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView10;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView11;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView12;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView13;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView14;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView15;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView16;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView17;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView18;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView19;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView20;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView21;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView22;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView23;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView24;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView25;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView26;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView27;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView28;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView29;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView30;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView31;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView32;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView33;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView34;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView35;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView36;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView37;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView38;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView39;
+
 - (IBAction)botonA:(id)sender;
 - (IBAction)botonB:(id)sender;
 - (IBAction)botonC:(id)sender;
--- HeroesDelPlanetaAzul/JuegoViewController.m
+++ HeroesDelPlanetaAzul/JuegoViewController.m
@@ -8,7 +8,14 @@
 
 #import "JuegoViewController.h"
 
-@interface JuegoViewController ()
+@interface JuegoViewController (){
+    NSMutableArray *letrasArray;
+    NSString *nombreFoto;
+}
+
+- (void)preparaArreglo;
+- (void)acomodaLetras;
+- (void)buscaLetra:(NSString*) letra;
 
 @end
 
@@ -26,17 +33,307 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self preparaArreglo];
+    
+    [self acomodaLetras];
     
    // self.fraseLabel.font = [UIFont fontWithName:@"SnackerComic_PersonalUseOnly"size:48];
     
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void) preparaArreglo{
+    
+    if (!letrasArray) {
+        letrasArray = [[NSMutableArray alloc] init];
+    }
+    
+    NSString *frase = @"En la higiene y limpieza";
+    
+    frase = [frase uppercaseString];
+    
+    //se guarda la frase en un arreglo donde cada casilla tiene una letra o espacio en blanco
+    [frase enumerateSubstringsInRange:NSMakeRange(0, [frase length])
+                              options:(NSStringEnumerationByComposedCharacterSequences)
+                           usingBlock:^(NSString *substring, NSRange substringRange, NSRange enclosingRange, BOOL *stop) {
+                               [letrasArray addObject:substring];
+                           }];
 }
+
+- (void)acomodaLetras{
+    
+    //acomoda los lables para que desplieguen las letras del arreglo
+    for (int i = 0; i < letrasArray.count; i++){
+        
+        if([[letrasArray objectAtIndex:i] isEqualToString:@" "]){
+            //nombreFoto = [NSString stringWithFormat:@""];
+        }
+        else{
+            nombreFoto = [NSString stringWithFormat:@"espacioVacio.png"];
+        
+            switch (i) {
+                case 0:
+                    self.imageView1.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 1:
+                    self.imageView2.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 2:
+                    self.imageView3.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 3:
+                    self.imageView4.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 4:
+                    self.imageView5.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 5:
+                    self.imageView6.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 6:
+                    self.imageView7.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 7:
+                    self.imageView8.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 8:
+                    self.imageView9.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 9:
+                    self.imageView10.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 10:
+                    self.imageView11.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 11:
+                    self.imageView12.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 12:
+                    self.imageView13.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 13:
+                    self.imageView14.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 14:
+                    self.imageView15.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 15:
+                    self.imageView16.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 16:
+                    self.imageView17.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 17:
+                    self.imageView18.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 18:
+                    self.imageView19.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 19:
+                    self.imageView20.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 20:
+                    self.imageView21.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 21:
+                    self.imageView22.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 22:
+                    self.imageView23.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 23:
+                    self.imageView24.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 24:
+                    self.imageView25.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 25:
+                    self.imageView26.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 26:
+                    self.imageView27.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 27:
+                    self.imageView28.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 28:
+                    self.imageView29.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 29:
+                    self.imageView30.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 30:
+                    self.imageView31.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 31:
+                    self.imageView32.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 32:
+                    self.imageView33.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 33:
+                    self.imageView34.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 34:
+                    self.imageView35.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 35:
+                    self.imageView36.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 36:
+                    self.imageView37.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 37:
+                    self.imageView38.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 38:
+                    self.imageView39.image = [UIImage imageNamed:nombreFoto];
+                    break;
+            }
+        }
+    }
+}
+
+
+- (void)buscaLetra:(NSString*) letra{
+    
+    for (int i = 0; i < letrasArray.count; i++){
+        
+        if([[letrasArray objectAtIndex:i] isEqualToString:letra]){
+            
+            nombreFoto = [NSString stringWithFormat:@"espacio%@.png", letra];
+            
+            switch (i) {
+                case 0:
+                    self.imageView1.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 1:
+                    self.imageView2.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 2:
+                    self.imageView3.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 3:
+                    self.imageView4.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 4:
+                    self.imageView5.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 5:
+                    self.imageView6.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 6:
+                    self.imageView7.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 7:
+                    self.imageView8.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 8:
+                    self.imageView9.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 9:
+                    self.imageView10.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 10:
+                    self.imageView11.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 11:
+                    self.imageView12.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 12:
+                    self.imageView13.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 13:
+                    self.imageView14.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 14:
+                    self.imageView15.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 15:
+                    self.imageView16.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 16:
+                    self.imageView17.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 17:
+                    self.imageView18.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 18:
+                    self.imageView19.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 19:
+                    self.imageView20.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 20:
+                    self.imageView21.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 21:
+                    self.imageView22.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 22:
+                    self.imageView23.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 23:
+                    self.imageView24.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 24:
+                    self.imageView25.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 25:
+                    self.imageView26.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 26:
+                    self.imageView27.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 27:
+                    self.imageView28.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 28:
+                    self.imageView29.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 29:
+                    self.imageView30.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 30:
+                    self.imageView31.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 31:
+                    self.imageView32.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 32:
+                    self.imageView33.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 33:
+                    self.imageView34.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 34:
+                    self.imageView35.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 35:
+                    self.imageView36.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 36:
+                    self.imageView37.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 37:
+                    self.imageView38.image = [UIImage imageNamed:nombreFoto];
+                    break;
+                case 38:
+                    self.imageView39.image = [UIImage imageNamed:nombreFoto];
+                    break;
+            }
+        }
+        
+    }
+}
+
+
+- (void)didReceiveMemoryWarning {
+    
+    [super didReceiveMemoryWarning];
+    
+}
+
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
@@ -47,144 +344,164 @@
     }
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+
+
+#pragma mark - Botones
 
 - (IBAction)botonA:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"A"];
 }
 
 - (IBAction)botonB:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"B"];
 }
 
 - (IBAction)botonC:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"C"];
 }
 
 - (IBAction)botonD:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"D"];
 }
 
 - (IBAction)botonE:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"E"];
 }
 
 - (IBAction)botonF:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"F"];
 }
 
 - (IBAction)botonG:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"G"];
 }
 
 - (IBAction)botonH:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"H"];
 }
 
 - (IBAction)botonI:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"I"];
 }
 
 - (IBAction)botonJ:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"J"];
 }
 
 - (IBAction)botonK:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"K"];
 }
 
 - (IBAction)botonL:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"L"];
 }
 
 - (IBAction)botonM:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"M"];
 }
 
 - (IBAction)botonN:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"N"];
 }
 
 - (IBAction)botonO:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"O"];
 }
 
 - (IBAction)botonP:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"P"];
 }
 
 - (IBAction)botonQ:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"Q"];
 }
 
 - (IBAction)botonR:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"R"];
 }
 
 - (IBAction)botonS:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"S"];
 }
 
 - (IBAction)botonT:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"T"];
 }
 
 - (IBAction)botonU:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"U"];
 }
 
 - (IBAction)botonV:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"V"];
 }
 
 - (IBAction)botonW:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"W"];
 }
 
 - (IBAction)botonX:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"X"];
 }
 
 - (IBAction)botonY:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"Y"];
 }
 
 - (IBAction)botonZ:(id)sender {
     UIButton *theButton = (UIButton *)sender;
     theButton.enabled = NO;
+    [self buscaLetra:@"Z"];
 }
 @end
